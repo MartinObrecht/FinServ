6a1b2462b3a071e44ca41f8ae067a05b4e32eab6
MartinObrecht <martin.obrecht@yahoo.com.br>
2024-06-19 21:35:48 -0300

    feat: commit test

--- src/FinServ.Api/Program.cs
+++ src/FinServ.Api/Program.cs
@@ -16,12 +16,9 @@ namespace FinServ.Api
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
 
             app.UseHttpsRedirection();
 
